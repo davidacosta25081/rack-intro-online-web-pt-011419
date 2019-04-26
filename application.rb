1fe704399c1db0caabc9263f7a235ac398319b16
David Acosta <davidacosta25081@yahoo.com>
2019-04-26 05:24:16 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World, my name is David Acosta"
+    resp.write "Hello, my name is David"
     resp.finish
   end
 
